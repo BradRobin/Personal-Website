cbee80025b7558e241cd89dfa641b705a6a00434
BRAD ROBINSON <113350400+BradRobin@users.noreply.github.com>
2025-03-31 08:44:23 +0300

    gallery scroll update

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const imageScroll = document.querySelector(".image-scroll");
+    const container = document.querySelector(".image-scroll-container");
     let scrollAmount = 0;
-    let scrollSpeed = 1; // Adjust speed if needed
+    let scrollSpeed = 2; 
 
     function autoScroll() {
         scrollAmount += scrollSpeed;
